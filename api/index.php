7752712b355c00cc0118b9bde3ff6580d19b0a55
Dhruvkhorava <dkhorava@icrafttechnologies.com>
2026-09-17 14:16:45 +0530

    vercel add

--- api/index.php
+++ api/index.php
@@ -1,4 +1,9 @@
 <?php
 
+// Ensure serverless temporary views cache directory exists
+if (!is_dir('/tmp/views')) {
+    mkdir('/tmp/views', 0777, true);
+}
+
 // Forward Vercel requests to Laravel's public index
 require __DIR__ . '/../public/index.php';
